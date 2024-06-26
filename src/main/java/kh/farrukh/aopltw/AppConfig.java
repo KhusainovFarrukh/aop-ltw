b407c589f231d56286537906f36551616682c337
f_khusainov <farrukhbekkhusainov@gmail.com>
2024-06-26 14:50:24 +0500

    Removed unnecessary code

--- src/main/java/kh/farrukh/aopltw/AppConfig.java
+++ src/main/java/kh/farrukh/aopltw/AppConfig.java
@@ -1,11 +1,9 @@
 package kh.farrukh.aopltw;
 
-import org.springframework.beans.factory.annotation.Configurable;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.EnableLoadTimeWeaving;
 
 @Configuration
-@Configurable
 @EnableLoadTimeWeaving
 public class AppConfig {
 
