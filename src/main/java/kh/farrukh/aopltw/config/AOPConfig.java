70fb9af210928880a618075752c08cecf087f37c
f_khusainov <farrukhbekkhusainov@gmail.com>
2024-06-26 09:50:41 +0500

    First attempt using Load-time weaving

---
+++ src/main/java/kh/farrukh/aopltw/config/AOPConfig.java
@@ -0,0 +1,10 @@
+package kh.farrukh.aopltw.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.EnableLoadTimeWeaving;
+
+@Configuration
+@EnableLoadTimeWeaving
+public class AOPConfig {
+
+}
--- src/main/java/kh/farrukh/aopltw/tenant/TenantAspect.java
+++ src/main/java/kh/farrukh/aopltw/tenant/TenantAspect.java
@@ -1,18 +1,15 @@
 package kh.farrukh.aopltw.tenant;
 
-import jakarta.persistence.EntityManager;
 import org.aspectj.lang.annotation.AfterReturning;
 import org.aspectj.lang.annotation.Aspect;
 import org.hibernate.Session;
-import org.springframework.stereotype.Component;
 
 @Aspect
-@Component
 public class TenantAspect {
 
-  @AfterReturning(pointcut = "bean(entityManagerFactory) && execution(* createEntityManager(..))", returning = "entityManager")
-  public void getSessionAfter(EntityManager entityManager) {
-    entityManager.unwrap(Session.class)
+  @AfterReturning(pointcut = "execution (* org.hibernate.internal.SessionFactoryImpl.SessionBuilderImpl.openSession(..))", returning = "session")
+  public void getSessionAfter(Session session) {
+    session
         .enableFilter("tenantFilter")
         .setParameter("tenant", CurrentTenant.get());
   }
